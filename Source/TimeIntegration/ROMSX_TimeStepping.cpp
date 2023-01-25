9e2ae6cf8aab1dc35f1c11dfb86555d9abbce47f
Jean M. Sexton <jmsexton@lbl.gov>
2023-01-25 14:42:15 -0800

    Force no updates to uv

--- Source/TimeIntegration/ROMSX_TimeStepping.cpp
+++ Source/TimeIntegration/ROMSX_TimeStepping.cpp
@@ -66,4 +66,90 @@ ROMSX::Advance (int lev, Real time, Real dt_lev, int /*iteration*/, int /*ncycle
     MultiFab::Copy(V_new,V_old,0,0,V_new.nComp(),0);
     MultiFab::Copy(W_new,W_old,0,0,W_new.nComp(),0);
 
-}
+    //pre_step3d corrections to boundaries
+
+    //-----------------------------------------------------------------------
+    //  Time step momentum equation in the XI-direction.
+    //-----------------------------------------------------------------------
+
+    //  Time step right-hand-side terms.
+    //            u(i,j,k,nnew)=u(i,j,k,nnew)+                                &
+    //     &                    DC(i,0)*ru(i,j,k,nrhs)
+
+    //  Backward substitution.
+    //            u(i,j,k,nnew)=u(i,j,k,nnew)+cff
+
+    //  Compute new solution by back substitution.
+    //            u(i,j,k,nnew)=DC(i,k)
+
+    //  Compute new solution by back substitution.
+    //            u(i,j,k,nnew)=DC(i,k)
+
+    //  Couple and update new solution.
+    //            u(i,j,k,nnew)=u(i,j,k,nnew)-DC(i,0)
+    //# ifdef MASKING
+    //            u(i,j,k,nnew)=u(i,j,k,nnew)*umask(i,j)
+    //# endif
+    //# ifdef WET_DRY
+    //            u(i,j,k,nnew)=u(i,j,k,nnew)*umask_wet(i,j)
+    //            ru(i,j,k,nrhs)=ru(i,j,k,nrhs)*umask_wet(i,j)
+    //# endif
+
+    //-----------------------------------------------------------------------
+    //  Time step momentum equation in the ETA-direction.
+    //-----------------------------------------------------------------------
+    //  Time step right-hand-side terms.
+    //                v(i,j,k,nnew)=v(i,j,k,nnew)+DC(i,0)*rv(i,j,k,nrhs)
+
+    //  Backward substitution.
+    //            v(i,j,k,nnew)=v(i,j,k,nnew)+cff
+
+    //  Compute new solution by back substitution.
+    //            v(i,j,N(ng),nnew)=DC(i,N(ng))
+
+    //  Compute new solution by back substitution.
+    //              v(i,j,k,nnew)=DC(i,k)
+    //back-substition happens differently if OMEGA_IMPLICIT
+
+    //  Couple and update new solution.
+    //              v(i,j,k,nnew)=v(i,j,k,nnew)-DC(i,0)
+    //# ifdef MASKING
+    //              v(i,j,k,nnew)=v(i,j,k,nnew)*vmask(i,j)
+    //# endif
+    //# ifdef WET_DRY
+    //              v(i,j,k,nnew)=v(i,j,k,nnew)*vmask_wet(i,j)
+    //              rv(i,j,k,nrhs)=rv(i,j,k,nrhs)*vmask_wet(i,j)
+    //# endif
+
+    //    # ifdef UV_WAVEDRAG
+    //                  u(i,j,k,nnew)=u(i,j,k,nstp)-dt(ng)*cff*               &
+    // &                (u(i,j,k,nnew)-TIDES(ng)%filt_ubot(i,j))
+
+    //-----------------------------------------------------------------------
+    //  Set lateral boundary conditions.
+    //-----------------------------------------------------------------------
+    //      CALL u3dbc_tile (ng, tile,                                        &
+    //      CALL v3dbc_tile (ng, tile,                                        &
+
+    //-----------------------------------------------------------------------
+    //  Apply momentum transport point sources (like river runoff), if any.
+    //-----------------------------------------------------------------------
+    //u(i,j,k,nnew)=SOURCES(ng)%Qsrc(is,k)*cff1
+    //v(i,j,k,nnew)=SOURCES(ng)%Qsrc(is,k)*cff1
+
+    //-----------------------------------------------------------------------
+    //  Couple 2D and 3D momentum equations.
+    //-----------------------------------------------------------------------
+
+    //  Replace only BOUNDARY POINTS incorrect vertical mean with more
+    //  accurate barotropic component, ubar=DU_avg1/(D*on_u). Recall that,
+    //  D=CF(:,0).
+
+    //  Replace only BOUNDARY POINTS incorrect vertical mean with more
+    //  accurate barotropic component, vbar=DV_avg1/(D*om_v).  Recall that,
+    //  D=CF(:,0).
+
+    //-----------------------------------------------------------------------
+    //  Exchange boundary data.
+    //-----------------------------------------------------------------------
+  }
