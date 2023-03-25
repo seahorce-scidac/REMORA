fc4354308051ac1fbad7cf061e1a6ba8bc60e4ec
Jean M. Sexton <jmsexton@lbl.gov>
2023-03-25 13:52:19 -0700

    Revert "Change h to be negative"
    This reverts commit 4bce8b6d7671e9ce2d8a2cae1d08077da9e83cda.
    
    Revert "Fix up Hz to use periodic fill"
    
    This reverts commit 973c24c221358c80c60bedd684e49f5a4347b4a4.
    
    Revert "Revert "Change h to be negative""
    
    This reverts commit b5734edcb0b3b7a1546fe2474f923281a62a8145.

--- Source/TimeIntegration/ROMSX_TimeStepping.cpp
+++ Source/TimeIntegration/ROMSX_TimeStepping.cpp
@@ -343,12 +343,25 @@ void ROMSX::romsx_advance(int level,
               //  Compute horizontal mass fluxes, Hz*u/n and Hz*v/m.
               //-----------------------------------------------------------------------
                 if(k+1<=N)
+                if(i-1>=-2)
+                    {
                   Huon(i,j,k)=0.5*(Hz(i,j,k)+Hz(i-1,j,k))*uold(i,j,k,nrhs)*
                 on_u(i,j,0);
+                    }
+                else
+                    Huon(i,j,k)=(Hz(i,j,k))*uold(i,j,k,nrhs)*
+                on_u(i,j,0);
               if(k+1<=N)
+                  if(j-1>=-2)
+                      {
                       Hvom(i,j,k)=0.5*(Hz(i,j,k)+Hz(i,j-1,k))*vold(i,j,k,nrhs)*
                           om_v(i,j,0);
+                      }
+                  else
+                      Hvom(i,j,k)=(Hz(i,j,k))*vold(i,j,k,nrhs)*
+                          om_v(i,j,0);
                     });
+
         amrex::ParallelFor(gbx1, ncomp,
         [=] AMREX_GPU_DEVICE (int i, int j, int k, int n)
             {
