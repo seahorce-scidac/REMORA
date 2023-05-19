c04c2a0570008d777b9652342697da7f03fced73
Hannah Klion <hannah.klion@gmail.com>
2023-05-19 10:53:43 -0700

    update box size in 2D advection

--- Source/TimeIntegration/ROMSX_advance_2d.cpp
+++ Source/TimeIntegration/ROMSX_advance_2d.cpp
@@ -107,7 +107,9 @@ ROMSX::advance_2d (int lev,
         ubxD.makeSlab(2,0);
         vbxD.makeSlab(2,0);
         Box gbx1D = bxD;
+        Box gbx2D = bxD;
         gbx1D.grow(IntVect(1,1,0));
+        gbx2D.grow(IntVect(2,2,0));
         //AKA
         //ubxD.setRange(2,0);
         //vbxD.setRange(2,0);
@@ -213,7 +215,7 @@ ROMSX::advance_2d (int lev,
         {
             Drhs(i,j,0)=zeta(i,j,0,krhs)+h(i,j,0);
         });
-        amrex::ParallelFor(gbx1D,
+        amrex::ParallelFor(gbx2D,
         [=] AMREX_GPU_DEVICE (int i, int j, int)
         {
             Real cff=.5*on_u(i,j,0);
@@ -221,7 +223,7 @@ ROMSX::advance_2d (int lev,
             Real cff1=i-1>=0 ? cff*(Drhs(i,j,0)+Drhs(i-1,j,0)) : on_u(i,j,0)*Drhs(i,j,0);
             DUon(i,j,0)=ubar(i,j,0,krhs)*cff1;
         });
-        amrex::ParallelFor(gbx1D,
+        amrex::ParallelFor(gbx2D,
         [=] AMREX_GPU_DEVICE (int i, int j, int)
         {
             Real cff=.5*om_v(i,j,0);
@@ -238,13 +240,13 @@ ROMSX::advance_2d (int lev,
         {
             Zt_avg1(i,j,0)=0.0;
         });
-        amrex::ParallelFor(gbx1D,
+        amrex::ParallelFor(gbx2D,
         [=] AMREX_GPU_DEVICE (int i, int j, int)
         {
             DU_avg1(i,j,0)=0.0;
             DU_avg2(i,j,0)=cff2*DUon(i,j,0);
         });
-        amrex::ParallelFor(gbx1D,
+        amrex::ParallelFor(gbx2D,
         [=] AMREX_GPU_DEVICE (int i, int j, int)
         {
             DV_avg1(i,j,0)=0.0;
@@ -261,13 +263,13 @@ ROMSX::advance_2d (int lev,
             if(k==0)
             Zt_avg1(i,j,0)=Zt_avg1(i,j,0)+cff1*zeta(i,j,0,krhs);
         });
-        amrex::ParallelFor(gbx1D,
+        amrex::ParallelFor(gbx2D,
         [=] AMREX_GPU_DEVICE (int i, int j, int k)
         {
             DU_avg1(i,j,0)=DU_avg1(i,j,0)+cff1*DUon(i,j,0);
             DU_avg2(i,j,0)=DU_avg2(i,j,0)+cff2*DUon(i,j,0);
         });
-        amrex::ParallelFor(gbx1D,
+        amrex::ParallelFor(gbx2D,
         [=] AMREX_GPU_DEVICE (int i, int j, int)
         {
             DV_avg1(i,j,0)=DV_avg1(i,j,0)+cff1*DVom(i,j,0);
@@ -280,12 +282,12 @@ ROMSX::advance_2d (int lev,
             cff2=weight2[my_iif];
         else
             cff2=Real(5.0)/Real(12.0)*weight2[my_iif];
-        amrex::ParallelFor(gbx1D,
+        amrex::ParallelFor(gbx2D,
         [=] AMREX_GPU_DEVICE (int i, int j, int)
         {
             DU_avg2(i,j,0)=DU_avg2(i,j,0)+cff2*DUon(i,j,0);
         });
-        amrex::ParallelFor(gbx1D,
+        amrex::ParallelFor(gbx2D,
         [=] AMREX_GPU_DEVICE (int i, int j, int)
         {
             DV_avg2(i,j,0)=DV_avg2(i,j,0)+cff2*DVom(i,j,0);
@@ -478,6 +480,8 @@ ROMSX::advance_2d (int lev,
         //-----------------------------------------------------------------------
         //
         rhs_2d(bxD, ubar, vbar, rhs_ubar, rhs_vbar, DUon, DVom, krhs, N);
+        //Print().SetPrecision(18) << FArrayBox(rhs_ubar) << std::endl;
+        //Print().SetPrecision(18) << FArrayBox(rhs_vbar) << std::endl;
 
 #ifdef UV_COR
         // Coriolis terms for 2d ubar, vbar added to rhs_ubar and rhs_vbar
