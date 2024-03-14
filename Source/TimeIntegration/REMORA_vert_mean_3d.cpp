8715229dbf8f59603e728643bf82ce23b645a78d
Hannah Klion <hannah.klion@gmail.com>
2024-03-13 17:39:40 -0700

    correctly use face-centered pm and pn

--- Source/TimeIntegration/REMORA_vert_mean_3d.cpp
+++ Source/TimeIntegration/REMORA_vert_mean_3d.cpp
@@ -34,7 +34,7 @@ REMORA::vert_mean_3d (const Box& phi_bx, const int ioff, const int joff,
     ParallelFor(makeSlab(phi_bx,2,0), [=] AMREX_GPU_DEVICE (int i, int j, int )
     {
         Real on_u_or_om_v = 2.0_rt / (dxlen(i-ioff,j-joff,0) + dxlen(i,j,0));
-        Real cff1=1.0_rt/(CF(i,j,-1)*(1.0_rt/dxlen(i,j,0)));
+        Real cff1=1.0_rt/(CF(i,j,-1)*(on_u_or_om_v));
         DC(i,j,-1) = (DC(i,j,-1)*(on_u_or_om_v) - Dphi_avg1(i,j,0))*cff1; // recursive
     });
 
