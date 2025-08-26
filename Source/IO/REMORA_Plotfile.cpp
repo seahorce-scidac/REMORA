14def888a0f7dece8a069bad8f8bf860311f990a
Hannah Klion <hannah.klion@gmail.com>
2025-08-25 19:18:30 -0700

    patch number of grow cells in cell centering velocities

--- Source/IO/REMORA_Plotfile.cpp
+++ Source/IO/REMORA_Plotfile.cpp
@@ -211,10 +211,13 @@ REMORA::WritePlotFile ()
         containerHasElement(plot_var_names, "vorticity") ) {
 
         for (int lev = 0; lev <= finest_level; ++lev) {
-            mf_cc_vel[lev].define(grids[lev], dmap[lev], AMREX_SPACEDIM, IntVect(1,1,1));
+            // NOTE/TODO: Vorticity looks better doing this with 1 grow cell (IntVect(1,1,1)) as last arugment and
+            // 1 as last argument to average_face_to_cellcenter. However, vels don't have grow cells in the z-
+            // direction so this causes an out of bounds. This is presumably fixable.
+            mf_cc_vel[lev].define(grids[lev], dmap[lev], AMREX_SPACEDIM, IntVect(0,0,0));
             mf_cc_vel[lev].setVal(0.0_rt); // zero out velocity in case we have any wall boundaries
             average_face_to_cellcenter(mf_cc_vel[lev],0,
-                                       Array<const MultiFab*,3>{xvel_new[lev],yvel_new[lev],zvel_new[lev]},1);
+                                       Array<const MultiFab*,3>{xvel_new[lev],yvel_new[lev],zvel_new[lev]},0);
             mf_cc_vel[lev].FillBoundary(geom[lev].periodicity());
         } // lev
 
