a73896649be05b5c736fd13fdcab443f8faa80ed
Jean M. Sexton <jmsexton@lbl.gov>
2026-05-18 10:09:19 -0700

    coupling: add REMORA one-step evolve hook for driver loop

--- Source/REMORA_Coupling.cpp
+++ Source/REMORA_Coupling.cpp
@@ -28,6 +28,49 @@ namespace {
 constexpr int SSTIndex = 0;
 }
 
+amrex::Real
+REMORA::EvolveOneStep (amrex::Real /*time*/, amrex::Real /*dt_request*/)
+{
+    Real cur_time = t_new[0];
+    const int step = istep[0];
+
+    if (cur_time >= stop_time) {
+        return Real(0.0);
+    }
+
+    ComputeDt();
+
+    int lev = 0;
+    int iteration = 1;
+    if (max_level == 0) {
+        timeStep(lev, cur_time, iteration);
+    } else {
+        timeStepML(cur_time, iteration);
+    }
+
+    cur_time += dt[0];
+
+    if ( (plot_int > 0      && (step+1 - last_plot_file_step) == plot_int         ) ||
+         (plot_int_time > 0 && (cur_time >= (last_plot_file_time + plot_int_time))) )
+    {
+        last_plot_file_step = step+1;
+        last_plot_file_time = cur_time;
+        WritePlotFile(step+1);
+        history_count++;
+    }
+
+    if ((check_int > 0 && (step+1 - last_check_file_step) == check_int)
+            || (check_int_time > 0 && cur_time >= (last_check_file_time + check_int_time))) {
+        last_check_file_step = step+1;
+        last_check_file_time = cur_time;
+        WriteCheckpointFile();
+    }
+
+    post_timestep(step, cur_time, dt[0]);
+
+    return dt[0];
+}
+
 void
 REMORA::PackSurfaceState (Vector<MultiFab*>& state, Real /*time*/)
 {
