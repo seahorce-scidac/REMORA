aa1a61c2d89614519e24df99307e11aa6db287bc
Feranmi Falodun <fmfalodun@lbl.gov>
2026-06-30 12:45:24 -0700

    Updated prob in Upwelling_Coupling

--- Exec/Upwelling_Coupling/prob.cpp
+++ Exec/Upwelling_Coupling/prob.cpp
@@ -1,5 +1,4 @@
 #include "prob.H"
-#include "REMORA_prob_common.H"
 
 #include "AMReX_ParmParse.H"
 #include "AMReX_MultiFab.H"
@@ -17,7 +16,9 @@ amrex_probinit(const amrex_real* problo, const amrex_real* probhi)
 }
 
 Problem::Problem(const amrex::Real* /*problo*/, const amrex::Real* /*probhi*/)
-{}
+{
+    // Problem-specific parameters can be parsed here if needed later.
+}
 
 /**
  * \brief Initializes bathymetry h and surface height Zeta
@@ -29,58 +30,40 @@ void Problem::init_analytic_bathymetry (
         amrex::MultiFab& mf_h)
 {
     auto geomdata = geom.data();
-    bool EWPeriodic = geomdata.isPeriodic(0);
-    bool NSPeriodic = geomdata.isPeriodic(1);
 
-    // Must not be doubly periodic, and must have terrain
-    AMREX_ALWAYS_ASSERT( !NSPeriodic || !EWPeriodic);
     AMREX_ALWAYS_ASSERT( !m_solverChoice.flat_bathymetry);
 
     mf_h.setVal(geomdata.ProbHi(2));
-
-    const int Lm = geom.Domain().size()[0];
-    const int Mm = geom.Domain().size()[1];
-
-    for ( MFIter mfi(mf_h, TilingIfNotGPU()); mfi.isValid(); ++mfi )
+    Print() << "Initializing coastal bathymetry" << std::endl;
+    for (MFIter mfi(mf_h, TilingIfNotGPU()); mfi.isValid(); ++mfi)
     {
-      Array4<Real> const& h  = (mf_h).array(mfi);
+        Array4<Real> const& h = mf_h.array(mfi);
 
-      Box bx = mfi.tilebox();
-      Box gbx = bx;
-      gbx.grow(mf_h.nGrowVect());
+        Box bx = mfi.tilebox();
+        Box gbx = bx;
+        gbx.grow(mf_h.nGrowVect());
 
-      Box gbxD = gbx;
-      gbxD.makeSlab(2,0);
+        Box gbxD = gbx;
+        gbxD.makeSlab(2,0);
 
-      Gpu::streamSynchronize();
+        Gpu::streamSynchronize();
 
-      if (NSPeriodic) {
+        ParallelFor(gbxD, [=] AMREX_GPU_DEVICE (int i, int j, int)
+        {
+            const auto prob_lo = geomdata.ProbLo();
+            const auto dx = geomdata.CellSize();
 
-          ParallelFor(gbxD, [=] AMREX_GPU_DEVICE (int i, int j, int )
-          {
-              int iFort = i+1; // (+1 is to match the Fortran indexing in ROMS)
+            const Real x = prob_lo[0] + (i + 0.5_rt) * dx[0];
+            const Real x0 = prob_lo[0];
+            const Real x1 = geomdata.ProbHi(0);
+            const Real h0 = 300.0_rt;
+            const Real h1 = 10.0_rt;
 
-              Real val1 = (iFort <= Lm/2.0) ? iFort : Lm+1-iFort;
-              val1 -= 0.5;
-              Real adj = geomdata.CellSize()[1]/1000.0_rt;
-
-              h(i,j,0) = std::min(-geomdata.ProbLo(2),(84.5_rt+66.526_rt*std::tanh((val1*adj-10.0_rt)/7.0_rt)));
-          });
-
-      } else if (EWPeriodic) {
-
-          ParallelFor(gbxD, [=] AMREX_GPU_DEVICE (int i, int j, int )
-          {
-              int jFort = j+1; // (+1 is to match the Fortran indexing in ROMS)
-
-              Real val1 = (jFort<=Mm/2.0) ? jFort : Mm+1-jFort;
-              val1 -= 0.5;
-              Real adj = geomdata.CellSize()[0]/1000.0_rt;
-
-              h(i,j,0) = std::min(-geomdata.ProbLo(2),(84.5_rt+66.526_rt*std::tanh((val1*adj-10.0_rt)/7.0_rt)));
-          });
-      }
-    } // mfi
+            Real depth = h0 + (h1 - h0) * (x - x0) / (x1 - x0);
+            depth = amrex::min(amrex::max(depth, h1), h0);
+            h(i,j,0) = depth;
+        });
+    }
 }
 
 /**
@@ -104,7 +87,7 @@ void Problem::init_analytic_prob(
         amrex::MultiFab& mf_xvel,
         amrex::MultiFab& mf_yvel)
 {
-    bool l_use_salt = m_solverChoice.use_salt;
+    const bool l_use_salt = m_solverChoice.use_salt;
 
     auto geomdata = geom.data();
     const int khi = geomdata.Domain().bigEnd()[2];
@@ -112,30 +95,50 @@ void Problem::init_analytic_prob(
     [[maybe_unused]] bool EWPeriodic = geomdata.isPeriodic(0);
     [[maybe_unused]] bool NSPeriodic = geomdata.isPeriodic(1);
 
-    auto T0 = m_solverChoice.T0;
     auto S0 = m_solverChoice.S0;
     for (MFIter mfi(mf_cons, TilingIfNotGPU()); mfi.isValid(); ++mfi)
     {
         const Box &bx = mfi.tilebox();
+	amrex::Print() << "Box is " << bx << "comparing to khi+1 " << khi+1 << std::endl;
         AMREX_ALWAYS_ASSERT(bx.length()[2] == khi+1);
 
-        Array4<      Real> const& state = mf_cons.array(mfi);
-        Array4<      Real> const& x_vel = mf_xvel.array(mfi);
-        Array4<      Real> const& y_vel = mf_yvel.array(mfi);
+        Array4<Real> const& state = mf_cons.array(mfi);
+        Array4<Real> const& x_vel = mf_xvel.array(mfi);
+        Array4<Real> const& y_vel = mf_yvel.array(mfi);
 
         Array4<const Real> const& z_r = remora.vec_z_r[lev]->const_array(mfi);
         ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
         {
-            const Real z = z_r(i,j,k);
+            // Convert model z coordinate to positive depth below the surface.
+            const Real depth = amrex::max(0.0_rt, -z_r(i, j, k));
 
-            state(i, j, k, Temp_comp) = 1.;
-
-            state(i,j,k,Temp_comp)=T0+8.0_rt*std::exp(z/50.0_rt);
-            if (l_use_salt) {
-                state(i,j,k,Salt_comp)=S0;
+            Real temp = 6.5_rt;
+            if (depth <= 20.0_rt) {
+                temp = 14.0_rt;
+            } else if (depth <= 50.0_rt) {
+                const Real alpha = (depth - 20.0_rt) / (50.0_rt - 20.0_rt);
+                temp = 14.0_rt + alpha * (10.0_rt - 14.0_rt);
+            } else if (depth <= 300.0_rt) {
+                const Real alpha = (depth - 50.0_rt) / (300.0_rt - 50.0_rt);
+                temp = 10.0_rt + alpha * (6.5_rt - 10.0_rt);
             }
 
-            // Set tracer = 0 everywhere
+            Real salt = 33.5_rt;
+            if (depth <= 50.0_rt) {
+                salt = 33.5_rt;
+            } else if (depth <= 150.0_rt) {
+                const Real alpha = (depth - 50.0_rt) / (150.0_rt - 50.0_rt);
+                salt = 33.5_rt + alpha * (34.3_rt - 33.5_rt);
+            } else {
+                const Real alpha = amrex::min((depth - 150.0_rt) / (300.0_rt - 150.0_rt), 1.0_rt);
+                salt = 34.3_rt + alpha * (34.6_rt - 34.3_rt);
+            }
+
+            state(i, j, k, Temp_comp) = temp;
+            if (l_use_salt) {
+                state(i, j, k, Salt_comp) = salt;
+            }
+
             state(i, j, k, Tracer_comp) = 0.0_rt;
         });
 
@@ -162,23 +165,23 @@ void Problem::init_analytic_vmix(
         REMORA const& remora,
         MultiFab& mf_Akv, MultiFab& mf_Akt)
 {
-    for ( MFIter mfi((mf_Akv), TilingIfNotGPU()); mfi.isValid(); ++mfi )
+    for (MFIter mfi((mf_Akv), TilingIfNotGPU()); mfi.isValid(); ++mfi)
     {
-      Array4<Real> const& Akv = (mf_Akv).array(mfi);
-      Array4<Real> const& Akt = (mf_Akt).array(mfi);
-      Array4<const Real> const& z_w = remora.vec_z_w[lev]->const_array(mfi);
-      Box bx = mfi.tilebox();
-      bx.grow(IntVect(NGROW,NGROW,0));
-      Gpu::streamSynchronize();
-      amrex::ParallelFor(bx,
-      [=] AMREX_GPU_DEVICE (int i, int j, int k)
-      {
-        Akv(i,j,k) = 2.0e-03_rt+8.0e-03_rt*std::exp(z_w(i,j,k)/150.0_rt);
+        Array4<Real> const& Akv = mf_Akv.array(mfi);
+        Array4<Real> const& Akt = mf_Akt.array(mfi);
+        Array4<const Real> const& z_w = remora.vec_z_w[lev]->const_array(mfi);
+        Box bx = mfi.tilebox();
+        bx.grow(IntVect(NGROW,NGROW,0));
+        Gpu::streamSynchronize();
+        amrex::ParallelFor(bx,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k)
+        {
+            Akv(i,j,k) = 2.0e-03_rt+8.0e-03_rt*std::exp(z_w(i,j,k)/150.0_rt);
 
-        Akt(i,j,k,Temp_comp) = 1.0e-6_rt;
-        Akt(i,j,k,Salt_comp) = 1.0e-6_rt;
-        Akt(i,j,k,Tracer_comp) = 0.0_rt;
-      });
+            Akt(i,j,k,Temp_comp) = 1.0e-6_rt;
+            Akt(i,j,k,Salt_comp) = 1.0e-6_rt;
+            Akt(i,j,k,Tracer_comp) = 0.0_rt;
+        });
     }
 }
 
@@ -191,26 +194,26 @@ void Problem::init_analytic_hmix(
         MultiFab& mf_visc2_r,
         MultiFab& mf_diff2)
 {
-    for ( MFIter mfi((mf_visc2_p), TilingIfNotGPU()); mfi.isValid(); ++mfi )
+    for (MFIter mfi((mf_visc2_p), TilingIfNotGPU()); mfi.isValid(); ++mfi)
     {
-      Array4<Real> const& visc2_p = (mf_visc2_p).array(mfi);
-      Array4<Real> const& visc2_r = (mf_visc2_r).array(mfi);
-      Array4<Real> const& diff2   = mf_diff2.array(mfi);
-      Box bx = mfi.tilebox();
-      bx.grow(IntVect(NGROW,NGROW,0));
-      Gpu::streamSynchronize();
+        Array4<Real> const& visc2_p = mf_visc2_p.array(mfi);
+        Array4<Real> const& visc2_r = mf_visc2_r.array(mfi);
+        Array4<Real> const& diff2   = mf_diff2.array(mfi);
+        Box bx = mfi.tilebox();
+        bx.grow(IntVect(NGROW,NGROW,0));
+        Gpu::streamSynchronize();
 
-      int ncomp = mf_diff2.nComp();
+        int ncomp = mf_diff2.nComp();
 
-      amrex::ParallelFor(makeSlab(bx,2,0), [=] AMREX_GPU_DEVICE (int i, int j, int )
-      {
-        visc2_p(i,j,0) = 5.0_rt;
-        visc2_r(i,j,0) = 5.0_rt;
+        amrex::ParallelFor(makeSlab(bx,2,0), [=] AMREX_GPU_DEVICE (int i, int j, int)
+        {
+            visc2_p(i,j,0) = 2.0_rt;
+            visc2_r(i,j,0) = 2.0_rt;
 
-        for (int n = 0; n < ncomp; n++) {
-            diff2(i,j,0,n) = 0.0_rt;
-        }
-      });
+            for (int n = 0; n < ncomp; n++) {
+                diff2(i,j,0,n) = 0.0_rt;
+            }
+        });
     }
 }
 
@@ -221,41 +224,18 @@ void Problem::init_analytic_smflux(
         REMORA const& remora,
         MultiFab& mf_sustr, MultiFab& mf_svstr)
 {
-    auto geomdata = geom.data();
-    bool EWPeriodic = geomdata.isPeriodic(0);
-    bool NSPeriodic = geomdata.isPeriodic(1);
-
-    //If we had wind stress and bottom stress we would need to set these:
-    Real pi = 3.14159265359_rt;
-    Real tdays=remora.get_t_old(lev)/Real(24.0*60.0*60.0);
-    Real dstart=0.0_rt;
-    Real windamp;
-    //It's possible these should be set to be nonzero only at the boundaries they affect
-
-    // Don't allow doubly periodic in this case
-    AMREX_ALWAYS_ASSERT( !NSPeriodic || !EWPeriodic);
-
-    // Flow in x-direction (EW):
-    if (NSPeriodic) {
-        mf_sustr.setVal(0.0_rt);
-    }
-    else if (EWPeriodic) {
-        if ((tdays-dstart)<=2.0)
-            windamp=-0.1_rt*Real(sin(pi*(tdays-dstart)/4.0_rt))/Real(m_solverChoice.rho0);
-        else
-            windamp=-0.1_rt/m_solverChoice.rho0;
-        mf_sustr.setVal(windamp);
-    }
-
-    // Flow in y-direction (NS):
-    if (NSPeriodic) {
-        if ((tdays-dstart)<=2.0)
-            windamp=-0.1_rt*Real(sin(pi*(tdays-dstart)/4.0_rt))/Real(m_solverChoice.rho0);
-        else
-            windamp=-0.1_rt/m_solverChoice.rho0;
-        mf_svstr.setVal(windamp);
-    }
-    else if(EWPeriodic) {
-        mf_svstr.setVal(0.0_rt);
+    for (MFIter mfi((mf_sustr), TilingIfNotGPU()); mfi.isValid(); ++mfi)
+    {
+      Array4<Real> const& sustr = (mf_sustr).array(mfi);
+      Array4<Real> const& svstr = (mf_svstr).array(mfi);
+      Box bx = mfi.tilebox();
+      bx.grow(IntVect(NGROW,NGROW,0));
+      Gpu::streamSynchronize();
+      amrex::ParallelFor(makeSlab(bx,2,0),
+      [=] AMREX_GPU_DEVICE (int i, int j, int)
+      {
+        sustr(i,j,0) = 0.0_rt;
+        svstr(i,j,0) = 0.0_rt;
+      });
     }
 }
