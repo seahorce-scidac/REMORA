82cfb24faa6aa8d57bad893cf7b4660c153ffd80
Hannah Klion <hannah.klion@gmail.com>
2026-08-12 10:25:43 -0700

    add update_rivers.cpp

---
+++ Source/TimeIntegration/REMORA_update_rivers.cpp
@@ -0,0 +1,19 @@
+#include <REMORA.H>
+
+using namespace amrex;
+
+/**
+ * @param[in] time   Time at which to get updated river variables
+ */
+void
+REMORA::update_rivers (Real time) {
+#ifdef REMORA_USE_NETCDF
+    river_source_transport->update_interpolated_to_time(time);
+    river_source_transportbar->update_interpolated_to_time(time);
+    for (int i_comp=0; i_comp < ncons; i_comp++) {
+        if (solverChoice.do_rivers_cons[i_comp]) {
+            river_source_cons[i_comp]->update_interpolated_to_time(time);
+        }
+    }
+#endif
+}
