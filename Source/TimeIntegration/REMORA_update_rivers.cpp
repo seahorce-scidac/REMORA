c9ea26204bfaad56208732ab7d375ca2f091e77e
Hannah Klion <hannah.klion@gmail.com>
2026-08-12 11:01:20 -0700

    fix unused

--- Source/TimeIntegration/REMORA_update_rivers.cpp
+++ Source/TimeIntegration/REMORA_update_rivers.cpp
@@ -6,7 +6,13 @@ using namespace amrex;
  * @param[in] time   Time at which to get updated river variables
  */
 void
-REMORA::update_rivers (Real time) {
+REMORA::update_rivers (
+#ifdef REMORA_USE_NETCDF
+        Real time)
+#else
+        Real /*time*/)
+#endif
+{
 #ifdef REMORA_USE_NETCDF
     river_source_transport->update_interpolated_to_time(time);
     river_source_transportbar->update_interpolated_to_time(time);
