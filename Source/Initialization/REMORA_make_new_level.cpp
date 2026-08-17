d086dc5249a7dce5ae58ce730fb88f22897aafaa
Hannah Klion <hannah.klion@gmail.com>
2026-08-17 13:55:09 -0700

    avoid null dereference and foextrap bathymetry

--- Source/Initialization/REMORA_make_new_level.cpp
+++ Source/Initialization/REMORA_make_new_level.cpp
@@ -92,7 +92,7 @@ REMORA::MakeNewLevelFromCoarse (int lev, Real time, const BoxArray& ba,
 
     if (lev > hires_grid_level) {
         FillCoarsePatch(lev, time, vec_h[lev].get(), vec_h[lev-1].get(),
-                        BCVars::cons_bc);
+                        foextrap_bc());
     } else {
         set_bathymetry_averaged_down(lev);
     }
@@ -277,8 +277,8 @@ REMORA::RemakeLevel (int lev, Real time, const BoxArray& ba, const DistributionM
 
     // Handle bathymetry separately
     if (lev > hires_grid_level) {
-        FillPatch(lev, time, tmp_h, GetVecOfPtrs(vec_h), BCVars::cons_bc, BdyVars::null,0,false,false);
-        FillPatch(lev, time, tmp_h, GetVecOfPtrs(vec_h), BCVars::cons_bc, BdyVars::null,1,false,false);
+        FillPatch(lev, time, tmp_h, GetVecOfPtrs(vec_h), foextrap_bc(), BdyVars::null,0,false,false);
+        FillPatch(lev, time, tmp_h, GetVecOfPtrs(vec_h), foextrap_bc(), BdyVars::null,1,false,false);
         std::swap(tmp_h,           *vec_h[lev]);
     } else {
         set_bathymetry_averaged_down(lev);
